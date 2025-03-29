224c263e733647417e01765d2e4dae99df581f2d
werimo <winstonyuri54@gmail.com>
2025-03-29 10:54:13 +0300

    add full bookmark property functionality

---
+++ property-pulse/app/api/bookmarks/route.js
@@ -0,0 +1,70 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+//GET  /api/bookmarks
+export const GET = async () => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("user ID is required", { status: 401 });
+    }
+    const { userId } = sessionUser;
+    //find user in database
+    const user = await User.findOne({ _id: userId });
+    //get user's bookmarks
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const { propertyId } = await request.json();
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("user ID is required", { status: 401 });
+    }
+    const { userId } = sessionUser;
+    //find user in database
+    const user = await User.findOne({ _id: userId });
+    //check if property is bookmarked
+    let isBookmarked = user.bookmarks.includes(propertyId);
+    let message;
+
+    if (isBookmarked) {
+      //if already bookmarked, remove it
+      user.bookmarks.pull(propertyId);
+      message = "Bookmark removed succesfully";
+      isBookmarked = false;
+    } else {
+      //if not bookmarked then add a bookmark.
+      user.bookmarks.push(propertyId);
+      message = "Bookmark added succesfully";
+      isBookmarked = true;
+    }
+
+    await user.save();
+    return new Response(JSON.stringify({ message, isBookmarked }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("something went wrong", { status: 500 });
+  }
+};
