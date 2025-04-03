f542c827df55dccac2c0d7adcc20f9d2399ac577
werimo <winstonyuri54@gmail.com>
2025-04-03 09:32:56 +0300

    add sort messages by recency

--- property-pulse/app/api/messages/route.js
+++ property-pulse/app/api/messages/route.js
@@ -17,9 +17,19 @@ export const GET = async () => {
       });
     }
     const { userId } = sessionUser;
-    const messages = await Message.find({ recipient: userId })
+    const readMessages = await Message.find({ recipient: userId, read: true })
+      .sort({ createdAt: -1 }) //sort read messaged in ascending order
       .populate("sender", "username")
       .populate("property", "name");
+    const unreadMessages = await Message.find({
+      recipient: userId,
+      read: false,
+    })
+      .sort({ createdAt: -1 }) //sort read messaged in ascending order
+      .populate("sender", "username")
+      .populate("property", "name");
+
+    const messages = [...unreadMessages, ...readMessages];
 
     return new Response(JSON.stringify(messages), { status: 200 });
   } catch (error) {
